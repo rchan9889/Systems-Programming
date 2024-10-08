4c4191b7f58a3b35c56f6dfe8a72dce19627e06d
Boyang Zhao <73202541+azbycxazbycx@users.noreply.github.com>
2024-10-08 02:49:51 -0400

    Update mymalloc.c

--- P1/mymalloc.c
+++ P1/mymalloc.c
@@ -58,38 +58,43 @@ static void initialize_heap(void){
 
 void *mymalloc(size_t size, char *file, int line){
     int offset = 0;
+    printf("Size input equals %ld\n", size);
     size = (size + 7) & ~7;
-    
+    printf("Size of size variable is %ld\n", size);
     if(size <= 0){        
-        printf("malloc: Unable to allocate %zu bytes (%c:%d)\n", size, *file, line);
+        printf("malloc: Unable to allocate %zu bytes (%c:%d) A\n", size, *file, line);
         return NULL;
     }
 
     if(size > MEMLENGTH){
-        printf("malloc: Unable to allocate %zu bytes (%c:%d)\n", size, *file, line);
+        printf("malloc: Unable to allocate %zu bytes (%c:%d) B\n", size, *file, line);
         return NULL;
     }
 
     struct node *curr_header;
     struct node *new_header;
-    struct node *next_header;
+    
 
     if(!initialized) initialize_heap();
 
     while (offset + 8 < MEMLENGTH) {
+	printf("Test statement, offset = %d\n", offset);
         curr_header = (struct node *) (heap.bytes + offset);
 
         // When current chunk isn't allocated and also big enough, we return first address in chunk 
         // and make new header if we can
-        if (!(curr_header->allocated) && curr_header->size >= size)  { 
+        if (!(curr_header->allocated) && curr_header->size >= size)  {
+	    	
             if ((offset + 8 + curr_header->size <= MEMLENGTH) && curr_header->size > size) {
                 new_header = (struct node *) (heap.bytes + offset + 8 + size);
                 new_header->size = curr_header->size - size - 8;
-            }
-            struct node *head = (struct node *) (heap.bytes + offset);
-            head->allocated = 1;
-            head->size = size;
-            return (struct node *) (heap.bytes + offset + 8);
+            	printf("Since current chunk has extra space after allocating, new chunk is created from address %d to address %d\n", (int) (offset + 8 + size), (int) (offset + 8 + curr_header->size));
+	    }
+            //struct node *head = (struct node *) (heap.bytes + offset);
+            curr_header->allocated = 1;
+            curr_header->size = size;
+	    printf("Memory of size %ld allocated from address %d to address %ld\n", size, (int) offset, offset + 8 + size);
+            return (heap.bytes + offset + 8);
         }
 
         // When current chunk isn't allocated but not big enough, we first check to see if we can coalesce
@@ -98,17 +103,27 @@ void *mymalloc(size_t size, char *file, int line){
 
             // Check if current chunk is last chunk. If it is, not enough space in heap
             if (offset + curr_header->size + 8 >= MEMLENGTH) {
-                printf("malloc: Unable to allocate %zu bytes (%c:%d)\n", size, *file, line);
+                printf("malloc: Unable to allocate %zu bytes (%c:%d) D\n", size, *file, line);
                 return NULL;
             }
             else {
-                next_header = (struct node *) (heap.bytes + offset + curr_header->size + 8);
+		printf("The current chunk from address %d to %d isn't big enough to fit size %d\n", (int) offset, (int) (offset + 8 + curr_header->size), (int) size);
+                //printf("Test next_header status: %d\n", next_header->allocated);
+		//printf("Test next_header size: %d\n", next_header->size);
+		struct node *next_header;
+		next_header = (struct node *) (heap.bytes + offset + curr_header->size + 8);
 
                 // Coalesces with next chunk if it is also not allocated
                 if (!(next_header->allocated)) {
                     curr_header->size += (8 + next_header->size);
-                    // TODO: Code that deletes the next header
-                }
+                    next_header = NULL;
+		    printf("Current chunk coalesced with next chunk to form chunk of size %d\n", curr_header->size);
+		}
+		else {
+		    printf("Next chunk is allocated already, so cannot coalesce chunks\n");
+		    offset += (curr_header->size + 8);	
+		}
+		
             }
 
         }
@@ -118,6 +133,7 @@ void *mymalloc(size_t size, char *file, int line){
             return NULL;
         }
         else {
+	    printf("Current chunk is already allocated, onto next one");
             offset += (curr_header->size + 8);
         }
         // If we can't coalesce chunks or current chunk is already allocated, we move on to the next header
@@ -148,6 +164,7 @@ void *mymalloc(size_t size, char *file, int line){
 }
 
 void myfree(void *ptr, char *file, int line){
+    printf("Attempting to free at address %ld\n", (char*) ptr - heap.bytes);
     if(initialized == 0){
         printf("free: Inappropriate pointer (%c:%d)\n", *file, line);
         exit(2);
@@ -177,6 +194,7 @@ void myfree(void *ptr, char *file, int line){
         printf("free: Inappropriate pointer (%c:%d)\n", *file, line);
         exit(2);
     }else{
+	printf("Successfully freed chunk from address %ld to address %ld\n",(char*) header - heap.bytes,(char*)  header - heap.bytes + header->size + 8);
         header->allocated = 0;
     }
 }
