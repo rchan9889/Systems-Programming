9a49bee743a078301f5827536d5149f7cbec9acc
Boyang Zhao <73202541+azbycxazbycx@users.noreply.github.com>
2024-11-03 01:19:54 -0400

    Update words.c

--- P2/words.c
+++ P2/words.c
@@ -56,7 +56,12 @@ char **split(const char *input) {
             }
         }
         else if (s[i] == 39) { // Current letter is an apostrophe
-            continue;
+            if (!(s[i-1] < 65 || ((s[i-1] > 90 && s[i-1] < 97) || s[i-1] > 122))) {
+                s[i] = ' ';
+            }
+            else if (!(s[i+1] < 65 || ((s[i+1] > 90 && s[i+1] < 97) || s[i+1] > 122))) {
+                s[i] = ' ';
+            }
         }
         else if ((s[i] < 65 || ((s[i] > 90 && s[i] < 97) || s[i] > 122))) {
                 s[i] = ' ';
