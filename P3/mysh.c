1270464917b87037d1133533d94872e2e95070f9
Boyang Zhao <73202541+azbycxazbycx@users.noreply.github.com>
2024-11-25 14:06:15 -0500

    Update mysh.c

--- P3/mysh.c
+++ P3/mysh.c
@@ -37,10 +37,10 @@ int main(int arc, char *argv){
                     if (command[i] != 10) {
                         path[i - 3] = command[i];
                     }
-		            i++;
+		    i++;
                 }
                 printf("%s\n", path);
-		        if(path[0] == 47) { // The ascii value for backslash
+		if(path[0] == 47) { // The ascii value for backslash
                     if(chdir(path) == -1){
                         printf("cd: No such file or directory\n");
                     }else{
@@ -49,13 +49,13 @@ int main(int arc, char *argv){
                         getcwd(cwd, sizeof(cwd));
                         printf("%s\n", cwd);
                     }
-		        }
-		        else{
-		            char cwd[PATH_MAX];
+		}
+		else{
+		    char cwd[PATH_MAX];
                     getcwd(cwd, sizeof(cwd));
                     char *filename = malloc(sizeof(cwd) + 1 + sizeof(path) + 1); // blah/blah + / + testfile + \0
                     
-		            strcat(filename, cwd);
+		    strcat(filename, cwd);
                     strcat(filename, "/");
                     strcat(filename, path);
 
@@ -67,62 +67,49 @@ int main(int arc, char *argv){
                         printf("%s\n", cwds);
                     }
 
-		    /*
-        	    char* dirOne = malloc(sizeof(char) * (15 + sizeof(path) + 1));
-		    strcat(dirOne, "/usr/local/bin");
-		    strcat(dirOne, path);
-		    char* dirTwo = malloc(sizeof(char) * (9 + sizeof(path) + 1));
-		    strcat(dirTwo, "/usr/bin/");
-		    strcat(dirTwo, path);
-		    char* dirThree = malloc(sizeof(char) * (5 + sizeof(path) + 1));
-                    strcat(dirThree, "/bin/");
-		    strcat(dirThree, path);
 		    
-		    printf("dirOne = _%s_\n", dirOne);
-		    printf("dirTwo = _%s_\n", dirTwo);
-		    printf("dirThree = _%s_\n", dirThree);
-		    if (access(dirOne, F_OK != -1)) {
-			if (chdir(dirOne) == -1) {
-			    printf("cd: No such file or directory\n");
-			}
-			else{
-			    char cwds[PATH_MAX];
-                            getcwd(cwds, sizeof(cwds));
-                            printf("%s\n", cwds);
-			}
-		    } 
-		    else if (access(dirTwo, F_OK != -1)) {
-                        if (chdir(dirTwo) == -1) {
-                            printf("cd: No such file or directory\n");
-                        }
-                        else{
-                            char cwds[PATH_MAX];
-                            getcwd(cwds, sizeof(cwds));
-                            printf("%s\n", cwds);
-                        }
-                    }
-		    else if (access(dirThree, F_OK != -1)) {
-                        if (chdir(dirThree) == -1) {
-                            printf("cd: No such file or directory\n");
-                        }
-                        else{
-                            char cwds[PATH_MAX];
-                            getcwd(cwds, sizeof(cwds));
-                            printf("%s\n", cwds);
-                        }
-                    }
-		    else{
-			printf("cd: No such file or directory\n");
-		    }  
-		    */
-		        }
+		}
                 memset(path, 0, sizeof(path));
             }else if(command[0] == 'p' && command[1] == 'w' && command[2] == 'd'){ //pwd
                 char cwd[PATH_MAX];
                 getcwd(cwd, sizeof(cwd));
                 printf("%s\n", cwd);
             }else if(command[0] == 'w' && command[1] == 'h' && command[2] == 'i' && command[3] == 'c' && command[4] == 'h' && command[5] == ' '){ //which
-                
+        	char path[PATH_MAX];
+                int i = 6;
+                while(command[i]){
+		    //printf("Testing: %d\n", i);	
+                    if (command[i] != 10) {
+                        path[i - 3] = command[i];
+		    }
+		    i++;
+                }
+		    
+		char* dirOne = malloc(sizeof(char) * (15 + sizeof(path) + 1));
+		strcat(dirOne, "/usr/local/bin/");
+		strcat(dirOne, path);
+		    
+		char* dirTwo = malloc(sizeof(char) * (9 + sizeof(path) + 1));
+		strcat(dirTwo, "/usr/bin/");
+		strcat(dirTwo, path);
+		    
+		char* dirThree = malloc(sizeof(char) * (5 + sizeof(path) + 1));
+                strcat(dirThree, "/bin/");
+		strcat(dirThree, path);
+		    
+		printf("dirOne = _%s_\n", dirOne);
+		printf("dirTwo = _%s_\n", dirTwo);
+		printf("dirThree = _%s_\n", dirThree);
+		if (access(dirOne, F_OK != -1)) {
+		    printf("%s\n", dirOne);
+		} 
+		else if (access(dirTwo, F_OK != -1)) {
+                    printf("%s\n", dirTwo);
+                }
+		else if (access(dirThree, F_OK != -1)) {
+                    printf("%s\n", dirThree);
+                } 
+		
             }else if(command[0] == 'e' && command[1] == 'x' && command[2] == 'i' && command[3] == 't'){ //exit
                 printf("exiting\n");
                 return 0;
