7e0aed4dd9ec21ac43ac38c52359e69fb4ed2993
Boyang Zhao <73202541+azbycxazbycx@users.noreply.github.com>
2024-11-29 22:17:25 -0500

    Update mysh.c

--- P3/mysh.c
+++ P3/mysh.c
@@ -85,7 +85,7 @@ int main(int arc, char *argv){
 		            i++;
                 }
 
-                /*
+                
                 char cwd[PATH_MAX];
                 getcwd(cwd, sizeof(cwd));
                 char *filename = malloc(sizeof(cwd) + 1 + sizeof(path) + 1); // blah/blah + / + testfile + \0
@@ -104,9 +104,9 @@ int main(int arc, char *argv){
                 free(filename);
                 memset(cwd, 0, sizeof(cwd));
                 memset(path, 0, sizeof(path));
-                */
-
                 
+
+                /*
                 char* dirOne = malloc(sizeof(char) * (15 + sizeof(path) + 1));
                 strcat(dirOne, "/usr/local/bin/");
                 strcat(dirOne, path);
@@ -133,6 +133,7 @@ int main(int arc, char *argv){
                 free(dirTwo);
                 free(dirThree);
                 memset(path, 0, sizeof(path));
+		*/
             }else if(command[0] == 'e' && command[1] == 'c' && command[2] == 'h' && command[3] == 'o') {
                 char text[PATH_MAX];
                 int i = 5;
